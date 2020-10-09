a986913b101449cef47d0e17efdcadbdcca66e34
Jakub Drak Sbahi <jakubdrak98@gmail.com>
2020-10-09 16:28:48 +0200

    Add routing

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
