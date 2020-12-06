d64341adc3d4b14c0cbfd81a9a158513b49ca9a1
Bartosz Chrostowski <b.chrostowski@student.mini.pw.edu.pl>
2020-12-06 04:06:44 +0100

    Remove redundant database query

--- backend/TreeOfAKind.Application/Command/Trees/TreeOperationCommandValidator.cs
+++ backend/TreeOfAKind.Application/Command/Trees/TreeOperationCommandValidator.cs
@@ -1,20 +1,14 @@
 ﻿using FluentValidation;
-using TreeOfAKind.Application.Command.Trees.AddTreeOwner;
 using TreeOfAKind.Application.Configuration;
-using TreeOfAKind.Domain.Trees;
 
 namespace TreeOfAKind.Application.Command.Trees
 {
     public class TreeOperationCommandValidator : AbstractValidator<TreeOperationCommand>
     {
-        private readonly ITreeRepository _treeRepository;
-        public TreeOperationCommandValidator(ITreeRepository treeRepository)
+        public TreeOperationCommandValidator()
         {
-            _treeRepository = treeRepository;
-            
             RuleFor(x => x.TreeId)
                 .NotEmpty()
-                .MustAsync(async (x, c) => (await _treeRepository.GetByIdAsync(x, c)) is {})
                 .WithMessage($"{nameof(TreeOperationCommand.TreeId)} is not valid.");
 
             RuleFor(x => x.RequesterUserAuthId)
