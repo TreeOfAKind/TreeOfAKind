66d1f38b337513d5af2151d9c0a1add53d1fd8ef
Jakub Drak Sbahi <jakubdrak98@gmail.com>
2020-12-16 11:36:02 +0100

    Display temporary people table in tree view

--- web/src/app/tree/tree-view/tree-view.component.ts
+++ web/src/app/tree/tree-view/tree-view.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Tree } from '../shared/tree.model';
+import { TreeService } from '../shared/tree.service';
 
 @Component({
   selector: 'app-tree-view',
@@ -7,14 +9,24 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./tree-view.component.scss']
 })
 export class TreeViewComponent implements OnInit {
+  model: Tree = {
+    treeId: null,
+    treeName: null,
+    photoUri: null,
+    people: []
+  };
   treeId: string;
 
   constructor(
+    private service: TreeService,
     private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
     this.treeId = this.route.snapshot.paramMap.get('id');
+    this.service.getTree(this.treeId).subscribe(result => {
+      this.model = result;
+    });
   }
 
 }
