72042adba5a29386c4ae5332877b845614a281de
Emil Draganczuk <emil.draganczuk@leancode.pl>
2020-12-27 17:01:10 +0100

    Add commands for adding and removing family members

--- mobile/lib/contracts/people/contracts.dart
+++ mobile/lib/contracts/people/contracts.dart
@@ -1,4 +1,5 @@
 import 'package:json_annotation/json_annotation.dart';
+import 'package:tree_of_a_kind/features/cqrs/cqrs_action.dart';
 
 part 'contracts.g.dart';
 
@@ -37,4 +38,50 @@ class PersonDTO {
       _$PersonDTOFromJson(json);
 
   Map<String, dynamic> toJson() => _$PersonDTOToJson(this);
+}
+
+@JsonSerializable()
+class AddPerson extends Command {
+  final String treeId;
+  final String name;
+  final String lastName;
+  final String gender;
+  final DateTime birthDate;
+  final DateTime deathDate;
+  final String description;
+  final String biography;
+  final String mother;
+  final String father;
+  final String spouse;
+
+  @override
+  String get endpointRoute => "People/AddPerson";
+
+  AddPerson(
+      {this.treeId,
+      this.name,
+      this.lastName,
+      this.gender,
+      this.birthDate,
+      this.deathDate,
+      this.description,
+      this.biography,
+      this.mother,
+      this.father,
+      this.spouse});
+
+  Map<String, dynamic> toJson() => _$AddPersonToJson(this);
+}
+
+@JsonSerializable()
+class RemovePerson extends Command {
+  String treeId;
+  String personId;
+
+  @override
+  String get endpointRoute => "People/RemovePerson";
+
+  RemovePerson({this.treeId, this.personId});
+
+  Map<String, dynamic> toJson() => _$RemovePersonToJson(this);
 }
--- mobile/lib/contracts/people/contracts.g.dart
+++ mobile/lib/contracts/people/contracts.g.dart
@@ -44,3 +44,52 @@ Map<String, dynamic> _$PersonDTOToJson(PersonDTO instance) => <String, dynamic>{
       'children': instance.children,
       'unknownRelations': instance.unknownRelations,
     };
+
+AddPerson _$AddPersonFromJson(Map<String, dynamic> json) {
+  return AddPerson(
+    treeId: json['treeId'] as String,
+    name: json['name'] as String,
+    lastName: json['lastName'] as String,
+    gender: json['gender'] as String,
+    birthDate: json['birthDate'] == null
+        ? null
+        : DateTime.parse(json['birthDate'] as String),
+    deathDate: json['deathDate'] == null
+        ? null
+        : DateTime.parse(json['deathDate'] as String),
+    description: json['description'] as String,
+    biography: json['biography'] as String,
+    mother: json['mother'] as String,
+    father: json['father'] as String,
+    spouse: json['spouse'] as String,
+  )..endpointRoute = json['endpointRoute'] as String;
+}
+
+Map<String, dynamic> _$AddPersonToJson(AddPerson instance) => <String, dynamic>{
+      'treeId': instance.treeId,
+      'name': instance.name,
+      'lastName': instance.lastName,
+      'gender': instance.gender,
+      'birthDate': instance.birthDate?.toIso8601String(),
+      'deathDate': instance.deathDate?.toIso8601String(),
+      'description': instance.description,
+      'biography': instance.biography,
+      'mother': instance.mother,
+      'father': instance.father,
+      'spouse': instance.spouse,
+      'endpointRoute': instance.endpointRoute,
+    };
+
+RemovePerson _$RemovePersonFromJson(Map<String, dynamic> json) {
+  return RemovePerson(
+    treeId: json['treeId'] as String,
+    personId: json['personId'] as String,
+  )..endpointRoute = json['endpointRoute'] as String;
+}
+
+Map<String, dynamic> _$RemovePersonToJson(RemovePerson instance) =>
+    <String, dynamic>{
+      'treeId': instance.treeId,
+      'personId': instance.personId,
+      'endpointRoute': instance.endpointRoute,
+    };
