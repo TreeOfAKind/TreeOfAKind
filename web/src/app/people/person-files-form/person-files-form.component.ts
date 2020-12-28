da5265eba87cb957d9f7038c366f2ffa8822d298
Jakub Drak Sbahi <jakubdrak98@gmail.com>
2020-12-28 14:21:22 +0100

    Add persons files

--- web/src/app/people/person-files-form/person-files-form.component.ts
+++ web/src/app/people/person-files-form/person-files-form.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TreeService } from 'src/app/tree/shared/tree.service';
+import { FileResponse } from '../shared/file-response.model';
 import { PeopleService } from '../shared/people.service';
+import { RemovePersonsFileRequest } from '../shared/remove-persons-file.model';
 
 @Component({
   selector: 'app-person-files-form',
@@ -12,6 +14,7 @@ export class PersonFilesFormComponent implements OnInit {
   treeId: string;
   personId: string;
   mainPhotoUrl: string;
+  files: FileResponse[];
 
   constructor(
     private service: PeopleService,
@@ -30,17 +33,52 @@ export class PersonFilesFormComponent implements OnInit {
       if(person.mainPhoto != null) {
         this.mainPhotoUrl = person.mainPhoto.uri;
       }
+      this.files = person.files;
     });
   }
 
   uploadPhoto(files: File[]) {
-    const formData: FormData = new FormData();
-    formData.append('treeId', this.treeId);
-    formData.append('personId', this.personId);
-    formData.append('file', files[0]);
+    const formData = this.prepareFormData(files[0]);
 
     this.service.changePersonsMainPhoto(formData).subscribe(response => {
       this.mainPhotoUrl = response.uri;
+    });
+  }
+
+  uploadFile(files: File[]) {
+    const formData = this.prepareFormData(files[0]);
+
+    this.service.addPersonsFile(formData).subscribe(res => {
+      this.updateFilesList();
+    });
+  }
+
+  deleteFile(file: FileResponse) {
+    const request: RemovePersonsFileRequest = {
+      treeId: this.treeId,
+      personId: this.personId ,
+      fileId: file.id
+    }
+
+    this.service.removePersonsFile(request).subscribe(res => {
+      this.updateFilesList();
+    })
+  }
+
+  private prepareFormData(file: File): FormData {
+    const formData: FormData = new FormData();
+    formData.append('treeId', this.treeId);
+    formData.append('personId', this.personId);
+    formData.append('file', file);
+
+    return formData;
+  }
+
+  private updateFilesList() {
+    this.treeService.getTree(this.treeId).subscribe(tree => {
+      const people = tree.people.filter(person => person.id !== this.personId);
+      const person = tree.people.find(per => per.id == this.personId);
+      this.files = person.files;
     });
   }
 }
