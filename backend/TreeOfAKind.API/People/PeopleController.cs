2556ec508953847d72a26f13b040611619baca66
Bartosz Chrostowski <b.chrostowski@student.mini.pw.edu.pl>
2020-12-28 02:44:07 +0100

    Update person

--- backend/TreeOfAKind.API/People/PeopleController.cs
+++ backend/TreeOfAKind.API/People/PeopleController.cs
@@ -7,12 +7,15 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TreeOfAKind.API.SeedWork;
+using TreeOfAKind.Application.Command.Trees.People;
 using TreeOfAKind.Application.Command.Trees.People.AddPerson;
 using TreeOfAKind.Application.Command.Trees.People.AddRelation;
 using TreeOfAKind.Application.Command.Trees.People.RemovePerson;
 using TreeOfAKind.Application.Command.Trees.People.RemoveRelation;
+using TreeOfAKind.Application.Command.Trees.People.UpdatePerson;
 using TreeOfAKind.Domain.Trees;
 using TreeOfAKind.Domain.Trees.People;
+using Relation = TreeOfAKind.Application.Command.Trees.People.Relation;
 
 namespace TreeOfAKind.API.People
 {
@@ -60,21 +63,21 @@ namespace TreeOfAKind.API.People
         {
             var authId = HttpContext.GetFirebaseUserAuthId();
 
-            var relations = new List<AddPersonCommand.Relation>();
+            var relations = new List<Relation>();
 
             if (request.Mother.HasValue)
             {
-                relations.Add(new AddPersonCommand.Relation(new PersonId(request.Mother.Value),
+                relations.Add(new Relation(new PersonId(request.Mother.Value),
                     RelationDirection.FromAddedPerson, RelationType.Mother));
             }
             if (request.Father.HasValue)
             {
-                relations.Add(new AddPersonCommand.Relation(new PersonId(request.Father.Value),
+                relations.Add(new Relation(new PersonId(request.Father.Value),
                     RelationDirection.FromAddedPerson, RelationType.Father));
             }
             if (request.Spouse.HasValue)
             {
-                relations.Add(new AddPersonCommand.Relation(new PersonId(request.Spouse.Value),
+                relations.Add(new Relation(new PersonId(request.Spouse.Value),
                     RelationDirection.FromAddedPerson, RelationType.Spouse));
             }
 
@@ -85,6 +88,68 @@ namespace TreeOfAKind.API.People
 
 
             return Created(string.Empty, new IdDto {Id = result.Value});
+        }
+
+        /// <summary>
+        /// Updates person in tree.
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST
+        ///     {
+        ///          "personId": "04520a87-6634-496a-9579-8fad94fba756",
+        ///          "treeId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        ///          "name": "Bartek",
+        ///          "lastName": "Chrostowski",
+        ///          "gender": "Male",
+        ///          "birthDate": "1998-12-04",
+        ///          "description": "Some guy",
+        ///          "biography": "H A T E S  his job 🍆🔫💣💀 at 🍑♻",
+        ///          "mother": "72bef03b-62c2-4829-9917-bed803397de5"
+        ///     }
+        /// </remarks>
+        /// <param name="request"></param>
+        /// <response code="200"></response>
+        /// <response code="400">Command is not valid</response>
+        /// <response code="401">User is not authenticated</response>
+        /// <response code="422">Business rule broken</response>
+        [HttpPost]
+        [Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        public async Task<IActionResult> UpdatePerson([FromBody] UpdatePersonRequest request)
+        {
+            var authId = HttpContext.GetFirebaseUserAuthId();
+
+            var relations = new List<Relation>();
+
+            if (request.Mother.HasValue)
+            {
+                relations.Add(new Relation(new PersonId(request.Mother.Value),
+                    RelationDirection.FromAddedPerson, RelationType.Mother));
+            }
+            if (request.Father.HasValue)
+            {
+                relations.Add(new Relation(new PersonId(request.Father.Value),
+                    RelationDirection.FromAddedPerson, RelationType.Father));
+            }
+
+            if (request.Spouse.HasValue)
+            {
+                relations.Add(new Relation(new PersonId(request.Spouse.Value),
+                    RelationDirection.FromAddedPerson, RelationType.Spouse));
+            }
+
+
+            var result = await _mediator.Send(
+                new UpdatePersonCommand(authId, new TreeId(request.TreeId), new PersonId(request.PersonId), request.Name, request.LastName, request.Gender,
+                    request.BirthDate, request.DeathDate, request.Description, request.Biography, relations));
+
+
+            return Ok();
         }
 
         /// <summary>
