0992c67583caa99769789598437f36660be8daed
Jakub Drak Sbahi <jakubdrak98@gmail.com>
2021-01-03 19:33:51 +0100

    Add drawing people from database

--- web/src/app/tree/tree-draw/tree-draw.component.ts
+++ web/src/app/tree/tree-draw/tree-draw.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import * as go from 'gojs';
+import { PersonResponse } from 'src/app/people/shared/person-response.model';
+import { DiagramMember } from '../shared/diagram-member.model';
+import { TreeService } from '../shared/tree.service';
 
 @Component({
   selector: 'app-tree-draw',
@@ -7,62 +10,41 @@ import * as go from 'gojs';
   styleUrls: ['./tree-draw.component.scss']
 })
 export class TreeDrawComponent implements OnInit {
+  @Input() treeId: string;
+  people: PersonResponse[];
 
-  constructor() {}
+  constructor(
+    private service: TreeService
+  ) {}
 
   ngOnInit() {
-    init();
+    this.service.getTree(this.treeId).subscribe(result => {
+      this.people = result.people;
+      this.convertPeopleToDiagramMembers();
+      init();
+    });
+  }
+
+  private convertPeopleToDiagramMembers() {
+    diagramMembers = [];
+    for(let i = 0; i < this.people.length; ++i) {
+      let person = this.people[i];
+      let diagramMember: DiagramMember = {
+        key: i,
+        n: `${person.name} ${person.lastName}`,
+        s: 'M',
+        m: person.mother ? this.people.findIndex(p => p.id === person.mother) : null,
+        f: person.father ? this.people.findIndex(p => p.id === person.father) : null,
+        ux: person.spouse ? this.people.findIndex(p => p.id === person.spouse) : null,
+        photo: person.mainPhoto ? person.mainPhoto.uri : '../../../assets/person-black-18dp.svg'
+      }
+      diagramMembers.push(diagramMember);
+    }
   }
 
 }
 
-var diagramMembers = [
-  { key: 0, n: "Aaron", s: "M", m: -10, f: -11, ux: 1, a: ["C", "F", "K"] },
-  { key: 1, n: "Alice", s: "F", m: -12, f: -13, a: ["B", "H", "K"] },
-  { key: 2, n: "Bob", s: "M", m: 1, f: 0, ux: 3, a: ["C", "H", "L"] },
-  { key: 3, n: "Barbara", s: "F", a: ["C"] },
-  { key: 4, n: "Bill", s: "M", m: 1, f: 0, ux: 5, a: ["E", "H"] },
-  { key: 5, n: "Brooke", s: "F", a: ["B", "H", "L"] },
-  { key: 6, n: "Claire", s: "F", m: 1, f: 0, a: ["C"] },
-  { key: 7, n: "Carol", s: "F", m: 1, f: 0, a: ["C", "I"] },
-  { key: 8, n: "Chloe", s: "F", m: 1, f: 0, vir: 9, a: ["E"] },
-  { key: 9, n: "Chris", s: "M", a: ["B", "H"] },
-  { key: 10, n: "Ellie", s: "F", m: 3, f: 2, a: ["E", "G"] },
-  { key: 11, n: "Dan", s: "M", m: 3, f: 2, a: ["B", "J"] },
-  { key: 12, n: "Elizabeth", s: "F", vir: 13, a: ["J"] },
-  { key: 13, n: "David", s: "M", m: 5, f: 4, a: ["B", "H"] },
-  { key: 14, n: "Emma", s: "F", m: 5, f: 4, a: ["E", "G"] },
-  { key: 15, n: "Evan", s: "M", m: 8, f: 9, a: ["F", "H"] },
-  { key: 16, n: "Ethan", s: "M", m: 8, f: 9, a: ["D", "K"] },
-  { key: 17, n: "Eve", s: "F", vir: 16, a: ["B", "F", "L"] },
-  { key: 18, n: "Emily", s: "F", m: 8, f: 9 },
-  { key: 19, n: "Fred", s: "M", m: 17, f: 16, a: ["B"] },
-  { key: 20, n: "Faith", s: "F", m: 17, f: 16, a: ["L"] },
-  { key: 21, n: "Felicia", s: "F", m: 12, f: 13, a: ["H"] },
-  { key: 22, n: "Frank", s: "M", m: 12, f: 13, a: ["B", "H"] },
-
-  // "Aaron"'s ancestors
-  { key: -10, n: "Paternal Grandfather", s: "M", m: -33, f: -32, ux: -11, a: ["A", "S"] },
-  { key: -11, n: "Paternal Grandmother", s: "F", a: ["E", "S"] },
-  { key: -32, n: "Paternal Great", s: "M", ux: -33, a: ["F", "H", "S"] },
-  { key: -33, n: "Paternal Great", s: "F", a: ["S"] },
-  { key: -40, n: "Great Uncle", s: "M", m: -33, f: -32, a: ["F", "H", "S"] },
-  { key: -41, n: "Great Aunt", s: "F", m: -33, f: -32, a: ["B", "I", "S"] },
-  { key: -20, n: "Uncle", s: "M", m: -11, f: -10, a: ["A", "S"] },
-
-  // "Alice"'s ancestors
-  { key: -12, n: "Maternal Grandfather", s: "M", ux: -13, a: ["D", "L", "S"] },
-  { key: -13, n: "Maternal Grandmother", s: "F", m: -31, f: -30, a: ["H", "S"] },
-  { key: -21, n: "Aunt", s: "F", m: -13, f: -12, a: ["C", "I"] },
-  { key: -22, n: "Uncle", s: "M", ux: -21 },
-  { key: -23, n: "Cousin", s: "M", m: -21, f: -22 },
-  { key: -30, n: "Maternal Great", s: "M", ux: -31, a: ["D", "J", "S"] },
-  { key: -31, n: "Maternal Great", s: "F", m: -50, f: -51, a: ["B", "H", "L", "S"] },
-  { key: -42, n: "Great Uncle", s: "M", m: -30, f: -31, a: ["C", "J", "S"] },
-  { key: -43, n: "Great Aunt", s: "F", m: -30, f: -31, a: ["E", "G", "S"] },
-  { key: -50, n: "Maternal Great Great", s: "F", ux: -51, a: ["D", "I", "S"] },
-  { key: -51, n: "Maternal Great Great", s: "M", a: ["B", "H", "S"] }
-];
+var diagramMembers: DiagramMember[];
 
 // gojs code
 
@@ -169,8 +151,8 @@ function init() {
       { locationSpot: go.Spot.Center, locationObjectName: "ICON", selectionObjectName: "ICON" },
       $(go.Panel,
         { name: "ICON" },
-        $(go.Shape, "Square",
-          { width: 40, height: 40, strokeWidth: 2, fill: "white", stroke: "#919191", portId: "" }),
+        $(go.Picture, {width: 80, height: 100},
+          new go.Binding("source", "photo")),
         $(go.Panel,
           { // for each attribute show a Shape at a particular place in the overall square
             itemTemplate:
@@ -186,7 +168,7 @@ function init() {
         )
       ),
       $(go.TextBlock,
-        { textAlign: "center", maxSize: new go.Size(80, NaN) },
+        { textAlign: "center", maxSize: new go.Size(80, NaN), margin: 5 },
         new go.Binding("text", "n"))
     ));
 
@@ -240,7 +222,7 @@ function init() {
 
   // n: name, s: sex, m: mother, f: father, ux: wife, vir: husband, a: attributes/markers
   setupDiagram(myDiagram, diagramMembers,
-    4 /* focus on this person */);
+    diagramMembers[0].key /* focus on this person */);
 }
 
 
@@ -296,7 +278,7 @@ function setupMarriages(diagram) {
     var data = nodeDataArray[i];
     var key = data.key;
     var uxs = data.ux;
-    if (uxs !== undefined) {
+    if (uxs != null) {
       if (typeof uxs === "number") uxs = [uxs];
       for (var j = 0; j < uxs.length; j++) {
         var wife = uxs[j];
@@ -317,7 +299,7 @@ function setupMarriages(diagram) {
       }
     }
     var virs = data.vir;
-    if (virs !== undefined) {
+    if (virs != null) {
       if (typeof virs === "number") virs = [virs];
       for (var j = 0; j < virs.length; j++) {
         var husband = virs[j];
@@ -349,16 +331,31 @@ function setupParents(diagram) {
     var key = data.key;
     var mother = data.m;
     var father = data.f;
-    if (mother !== undefined && father !== undefined) {
+    if (mother != null && father != null) {
       var link = findMarriage(diagram, mother, father);
       if (link === null) {
         // or warn no known mother or no known father or no known marriage between them
-        if (window.console) window.console.log("unknown marriage: " + mother + " & " + father);
+        if (mother != null) {
+          var cdata = { from: mother, to: key };
+          myDiagram.model.addLinkData(cdata);
+        }
+        if (father != null) {
+          var cdata = { from: father, to: key };
+          myDiagram.model.addLinkData(cdata);
+        }
         continue;
       }
       var mdata = link.data;
       var mlabkey = mdata.labelKeys[0];
       var cdata = { from: mlabkey, to: key };
+      myDiagram.model.addLinkData(cdata);
+    }
+    else if (mother != null) {
+      var cdata = { from: mother, to: key };
+      myDiagram.model.addLinkData(cdata);
+    }
+    else if (father != null) {
+      var cdata = { from: father, to: key };
       myDiagram.model.addLinkData(cdata);
     }
   }
