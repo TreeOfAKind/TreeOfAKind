ee3ec2a9437ed720a799b10524a76acd6952e32c
Emil Draganczuk <emil.draganczuk@leancode.pl>
2021-01-03 15:47:36 +0100

    Add logo and splash screen

--- mobile/lib/resources/images.dart
+++ mobile/lib/resources/images.dart
@@ -1,1 +1,5 @@
-class Images {}
+class Images {
+  static const String logoWithName =
+      'assets/logo_with_name.svg'; // Seems it can't be parsed by svg lib
+  static const String logoWithoutName = 'assets/logo_without_name.svg';
+}
