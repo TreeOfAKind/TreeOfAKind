7ccb475e1430d748c80a44125d3b3def8fd23199
Emil Draganczuk <emil.draganczuk@leancode.pl>
2021-01-05 20:11:19 +0100

    Enable uploading files

--- mobile/lib/features/person_files/view/person_files_page.dart
+++ mobile/lib/features/person_files/view/person_files_page.dart
@@ -1,3 +1,4 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tree_of_a_kind/contracts/people/contracts.dart';
@@ -29,6 +30,19 @@ class PersonFilesPage extends StatefulWidget {
 class _PersonFilesPageState extends State<PersonFilesPage> {
   String _getFullname(PersonDTO person) => '${person.name} ${person.lastName}';
 
+  Future<void> _pickFile(BuildContext context) async {
+    final result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['jpg', 'jpeg', 'png', 'pdf'],
+    );
+
+    final file = result?.files?.single;
+
+    if (file != null) {
+      BlocProvider.of<PersonFilesBloc>(context).add(FileAdded(file));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
@@ -36,7 +50,8 @@ class _PersonFilesPageState extends State<PersonFilesPage> {
     return Scaffold(
         appBar: AppBar(title: Text('${_getFullname(widget.person)} files')),
         floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.upload_file), onPressed: () => {}),
+            child: Icon(Icons.upload_file),
+            onPressed: () => _pickFile(context)),
         body: BlocBuilder<PersonFilesBloc, PersonFilesState>(
           builder: (context, state) {
             if (state is UnknownErrorState) {
