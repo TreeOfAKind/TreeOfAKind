4b12852547b5a8d24bf20ae3830cf4a7341c3ccd
Bartosz Chrostowski <b.chrostowski@student.mini.pw.edu.pl>
2021-01-07 11:28:00 +0100

    Fix adding tree owner if owner doesnt have an account

--- backend/TreeOfAKind.Infrastructure/Authentication/UserAuthIdProvider.cs
+++ backend/TreeOfAKind.Infrastructure/Authentication/UserAuthIdProvider.cs
@@ -1,19 +1,36 @@
-﻿using System.Net.Mail;
+﻿using System;
+using System.Net.Mail;
 using System.Threading;
 using System.Threading.Tasks;
 using FirebaseAdmin.Auth;
+using Serilog;
 using TreeOfAKind.Application.Services;
 
 namespace TreeOfAKind.Infrastructure.Authentication
 {
     public class UserAuthIdProvider : IUserAuthIdProvider
     {
+        private readonly ILogger _logger;
+
+        public UserAuthIdProvider(ILogger logger)
+        {
+            _logger = logger;
+        }
+
         public async Task<string> GetUserAuthId(MailAddress mailAddress, CancellationToken cancellationToken = default)
         {
-            var abstractFirebaseAuth =
-                await FirebaseAuth.DefaultInstance.GetUserByEmailAsync(mailAddress.Address, cancellationToken);
+            string uid = null;
+            try
+            {
+                 uid =
+                    (await FirebaseAuth.DefaultInstance.GetUserByEmailAsync(mailAddress.Address, cancellationToken))?.Uid;
+            }
+            catch (FirebaseAuthException)
+            {
+                _logger.Warning("User {@mail} not found in Firebase Auth database.", mailAddress);
+            }
 
-            return abstractFirebaseAuth.Uid;
+            return uid;
         }
     }
 }
